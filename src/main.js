e1c8a442e7d36778275dcb4775be41b6f78d2009
Robert Wood <L.robertwood@gmail.com>
2023-11-04 12:03:46 -0400

    bootstrap working

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import 'bootstrap/dist/css/bootstrap.css'; // Import Bootstrap CSS
+import 'bootstrap/dist/js/bootstrap.bundle'; // Import Bootstrap JavaScript
 createApp(App).mount('#app')
